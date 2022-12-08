02e8e615f9e3e0ebfe92ff639bfc3d97cc70f3fb
JohanLiebert <johanliebert.outergod@gmail.com>
2022-12-08 12:51:16 +0700

    add user auth using Next-Auth

--- components/User.tsx
+++ components/User.tsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import React from "react"
+import { useSession, signIn, signOut } from "next-auth/react"
 
 export default function User() {
+  const { data: session, status } = useSession()
+  if (status === "authenticated") {
+    return (
+      <>
+        <p onClick={() => signOut()}>{session.user?.name}</p>
+        <img src={session.user?.image} alt="" className="cursor-pointer h-10 w-10 rounded-full" />
+      </>
+    )
+  }
   return (
-    <div>User</div>
+    <>
+      <button onClick={() => signIn()} className="bg-blue-500 rounded-lg hover:drop-shadow-md text-white cursor-pointer px-7 py-3">SignIn</button>
+    </>
   )
 }
