10a0f38b7929510d7eca0c26cbb99da7742517a3
JohanLiebert <johanliebert.outergod@gmail.com>
2022-12-10 16:02:15 +0700

    try fix auth error

--- pages/api/[...nextauth].ts
+++ pages/api/[...nextauth].ts
@@ -5,7 +5,9 @@ export default NextAuth({
   // Configure one or more authentication providers
   providers: [
     GoogleProvider({
+      // @ts-ignore
       clientId: process.env.GOOGLE_CLIENT_ID,
+      // @ts-ignore
       clientSecret: process.env.GOOGLE_CLIENT_SECRET
     })
     // ...add more providers here
