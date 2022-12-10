7466dc63ff91099144b2db9545d17a16f17b069e
JohanLiebert <johanliebert.outergod@gmail.com>
2022-12-10 15:55:57 +0700

    try to fix auth error

--- pages/api/[...nextauth].ts
+++ pages/api/[...nextauth].ts
@@ -1,21 +1,17 @@
 import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
 
-export const authOptions = {
+export default NextAuth({
   // Configure one or more authentication providers
   providers: [
     GoogleProvider({
-      // @ts-ignore
       clientId: process.env.GOOGLE_CLIENT_ID,
-      // @ts-ignore
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    }),
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET
+    })
     // ...add more providers here
   ],
-  secret:process.env.SECRET,
-    pages:{
-        signIn: '/auth/signin',
-    }
-}
-
-export default NextAuth(authOptions)
+  secret: process.env.SECRET,
+  pages: {
+    signIn: "/auth/signin"
+  }
+})
