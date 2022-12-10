196d1d740c1b5cdd4787249204ace44a6abd2956
JohanLiebert <johanliebert.outergod@gmail.com>
2022-12-10 16:19:41 +0700

    change react stric

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  reactStrictMode: false,
+  reactStrictMode: true,
   images: {
     domains: ["lh3googleusercontent.com", "upload.wikimedia.org"]
   }
