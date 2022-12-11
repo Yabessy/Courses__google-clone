d8e54dcddbf193489ad7ebbfa0a37695fe9c09e7
JohanLiebert <johanliebert.outergod@gmail.com>
2022-12-11 17:10:32 +0700

    try fix auth error on deployment

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,9 +2,9 @@ import "../styles/globals.css"
 import type { AppProps } from "next/app"
 import { SessionProvider } from "next-auth/react"
 
-function MyApp({ Component, pageProps:{session, ...pageProps} }: AppProps) {
+function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
-    <SessionProvider session={session} basePath="api/auth">
+    <SessionProvider session={session}>
       <Component {...pageProps} />
     </SessionProvider>
   )
